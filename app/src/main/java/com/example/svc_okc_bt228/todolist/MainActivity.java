9d4b72ebdd58f4072c56892161434abfd2b73fe6
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-31 18:58:54 -0500

    Update app/src/main/java/com/example/svc_okc_bt228/todolist/MainActivity.java

--- app/src/main/java/com/example/svc_okc_bt228/todolist/MainActivity.java
+++ app/src/main/java/com/example/svc_okc_bt228/todolist/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.svc_okc_bt228.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//This is my first android studio experience
 public class MainActivity extends AppCompatActivity {
 
     @Override
